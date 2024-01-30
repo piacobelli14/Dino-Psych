5d0ee1258aa56848e37cdf5f9ef73b28796f6785
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-30 14:47:57 -0600

    update

--- dinopsychweb/src/pages/Login.jsx
+++ dinopsychweb/src/pages/Login.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import DinoLabsLogoWhite from "../assets/dinoLabsLogo_white.png";
+
+import "../styles/Login.css"
 
 const Login = ({ onRegister }) => {
   const navigate = useNavigate();
@@ -18,9 +22,31 @@ const Login = ({ onRegister }) => {
 
   return (
     <div>
+
+      <div className="loginHeaderContainer">
+        <div className="loginTopNavBarContainer"> 
+
+          <a className="loginSkipToContent"> 
+            <img className="loginLogo" src={DinoLabsLogoWhite} alt="Dino Labs Logo"/>
+
+            <label className="loginHeader">
+              Dino Psych
+            </label> 
+          </a>
+
+          <div className="hamburgerCircle">
+            <FontAwesomeIcon icon={faBars} className="hamburgerIcon" />
+          </div>
+        </div>
+
+        <div className="headerDivider"/>
+      </div>
+
+
+
      
 
-        {/*<img src={DinoLabsLogoWhite}/>*/}
+        
 
        
        
