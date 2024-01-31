835ae22affb80abc710dc2f4bc2c61f38c531bdc
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-31 16:46:05 -0600

    update

---
+++ dinopsychweb/src/pages/Manager.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"; 
+import { useNavigate } from "react-router-dom"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+
+import DinoLabsLogoWhite from "../assets/dinoLabsLogo_white.png"; 
+
+import "../styles/Manager.css"; 
+
+const Manager = () => {
+    return (
+        <div> 
+
+        </div>
+    );
+}; 
+
+export default Manager; 
