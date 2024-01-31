0dc71429ca260f61d856fce0f0ada53f87236ca3
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-30 20:26:21 -0600

    update

---
+++ DinoPsychAPI/dinopsych.js
@@ -0,0 +1,115 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const bodyParser = require('body-parser'); 
+const { Pool } = require('pg'); 
+const crypto = require('crypto'); 
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken'); 
+
+const app = express(); 
+const port = 3001; 
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '1gb' }));
+
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'];
+  
+    console.log(token); 
+  
+    if (token === undefined) {
+      return res.status(401).json({ message: 'Unauthorized: No token provided.' });
+    }
+  
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        console.error('JWT Verification Error:', err);
+        return res.status(403).json({ message: 'Unauthorized: Invalid token.' });
+      }
+      req.user = user;
+      next();
+    });
+}
+
+const username = 'piacobelli'
+const password = 'PAiac14-'
+const pool = new Pool({
+    user: username, 
+    host: 'localhost', 
+    database: 'dinolabs', 
+    password: password, 
+    port: '5551', 
+    max: 6, 
+    idleTimeoutMillis: 1000, 
+}); 
+
+module.exports = pool;
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err); 
+    process.exit(-1)
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+  
+      const passwordVerificationQuery = 'SELECT username, salt, hashedpassword, organizationid FROM dinolabsusers WHERE email = $1;';
+      const passwordVerificationResult = await pool.query(passwordVerificationQuery, [email]);
+  
+      if (passwordVerificationResult.error) {
+        return res.status(401).json({ message: 'Unable to verify login innfo at this time. Please try again.' });
+      }
+  
+      const row = passwordVerificationResult.rows[0];
+      if (!row) {
+        return res.status(401).json({ message: 'Unable to verify login innfo at this time. Please try again.' });
+      }
+  
+      const username = row.username; 
+      const storedSalt = row.salt;
+      const storedHashedPassword = row.hashedpassword;
+      const hashedPasswordToCheck = hashPassword(password, storedSalt);
+  
+      if (hashedPasswordToCheck === storedHashedPassword) {
+        const createLoginTokenQuery = 'INSERT INTO dinolabs_signintokens (username, signintimestamp, organizationid) VALUES ($1, NOW(), $2);';
+        const createLoginTokenResult = await pool.query(createLoginTokenQuery, [username, row.organizationid]);
+  
+        if (createLoginTokenResult.error) {
+            return res.status(401).json({ message: 'Unable to verify login innfo at this time. Please try again.' });
+        }
+  
+        const token = jwt.sign(
+          {
+            userId: username,
+          },
+          '1fafce907062c54ef898e72a20e2e4fab5275e639be048d1d79fdfa5b516459b',
+          {
+            expiresIn: '1h',
+          }
+        );
+
+        console.log(token, username); 
+  
+        return res.status(200).json({ token, username });
+      } else {
+        return res.status(401).json({ message: 'These login credentials are incorrect. Please try again.' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Error connecting to the database. Please try again later.' });
+    }
+});  
+
+function hashPassword(enteredPassword, storedSalt) {
+    if (!enteredPassword || !storedSalt) {
+        return null;
+    }
+
+    const saltedPasswordToCheck = storedSalt + enteredPassword; 
+    const hash = crypto.createHash('sha256');   
+    const hashedPassword = hash.update(saltedPasswordToCheck).digest('hex'); 
+    return hashedPassword;
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- dinopsychweb/src/pages/Login.jsx
+++ dinopsychweb/src/pages/Login.jsx
@@ -2,45 +2,153 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+import { faPerson } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 import DinoLabsLogoWhite from "../assets/dinoLabsLogo_white.png";
 
 import "../styles/Login.css"
 
-const Login = ({ onRegister }) => {
+const Login = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isHamburger, setIsHamburger] = useState(false); 
+  const [isEmail, setIsEmail] = useState(false); 
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [loginError, setLoginError] = useState("");
+
 
   useEffect(() => {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
   }, []);
 
-  
+  const handleLogin = async () => {
+    try {
+        const response = await fetch('http://172.20.10.3:3001/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("username", data.username);
+          navigate("/manager");
+        } else {
+          setLoginError('That username and/or password is incorrect.')
+        }
+    } catch (error) {
+      return;
+    }
+  };
 
   return (
     <div>
+
+      {isHamburger && (
+        <div className="hamburgerPopout"> 
+        </div>
+      )}
 
       <div className="loginHeaderContainer">
         <div className="loginTopNavBarContainer"> 
 
           <a className="loginSkipToContent"> 
             <img className="loginLogo" src={DinoLabsLogoWhite} alt="Dino Labs Logo"/>
-
             <label className="loginHeader">
               Dino Psych
             </label> 
           </a>
 
-          <div className="hamburgerCircle">
-            <FontAwesomeIcon icon={faBars} className="hamburgerIcon" />
-          </div>
+          <button className="hamburgerCircle" onClick={()=> setIsHamburger(!isHamburger)}>
+            <FontAwesomeIcon icon={isHamburger ? faX : faBars} className="hamburgerIcon" />
+          </button>
         </div>
 
         <div className="headerDivider"/>
+
+        {isHamburger && (
+          <button className="hamburgerLoginButton" onClick={()=> setIsHamburger(!isHamburger)} style={{"backgroundColor": "#2D3436", "color": "#F5F5F5"}}>
+              Login
+          </button>
+        )}
+
+        {isHamburger && (
+          <button className="hamburgerLoginButton" onClick={()=> navigate("/register")} style={{"backgroundColor": "#F5F5F5", "color": "#2D3436"}}>
+              Sign Up
+          </button>
+        )}
+
+
       </div>
+
+      <div className="loginBlock"> 
+        <label className="loginTitle">Log in to Dino Psych</label>
+
+        <div className="loginInputWrapper"> 
+          <input className="loginInput" type="text" placeholder={"Email Address"} onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+
+        {!isEmail && (
+          <button className="loginInputButton" style={{'backgroundColor': '#8884d8'}} onClick={()=> setIsEmail(!isEmail)}>
+            <FontAwesomeIcon icon={faEnvelope} className="envelopeIcon"/>
+            <label className="loginInputText">Continue with Email</label> 
+          </button>
+        )}
+
+        {!isEmail && (
+          <button className="loginInputButton" style={{'backgroundColor': '#2D3436'}} onClick={()=> navigate("/register")}>
+            <FontAwesomeIcon icon={faPerson} className="envelopeIcon"/>
+            <label className="loginInputText">Create an Account</label> 
+          </button>
+        )}
+
+        {!isEmail && (
+          <button className="loginSupplementalButton" onClick={()=> navigate("/reset")}>Forgot password? Click here to reset.</button>
+        )}
+
+        {isEmail && (
+          <div className="loginInputWrapper"> 
+            <input className="loginInputPassword" type={passwordVisible ? "text" : "password"} placeholder={"Password"} onChange={(e) => setPassword(e.target.value)}/>
+            <FontAwesomeIcon
+              icon={passwordVisible ? faEyeSlash : faEye}
+              onClick={() => setPasswordVisible(!passwordVisible)}
+              className="passwordToggleIcon"
+            />
+          </div>
+        )}
+
+        {isEmail && (
+          <button className="loginInputButton" style={{'backgroundColor': '#8884d8'}} onClick={handleLogin}>
+            <label className="loginInputText">Sign In</label> 
+          </button>
+        )}
+
+        <div className="loginError">{loginError}</div>
+
+        {isEmail && (
+          <button className="loginSupplementalButton" onClick={()=> setIsEmail(!isEmail)}>
+            <span>
+              <FontAwesomeIcon icon={faArrowLeft} className="envelopeIcon"/>
+            </span>
+            Return to Main
+          </button>
+        )}
+
+      </div>
+
+
 
 
 
