9fd23159c7ac0df43885a5f9ec22499b59268b11
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-31 12:09:34 -0600

    update

--- dinopsychweb/src/App.jsx
+++ dinopsychweb/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"; 
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Reset from "./pages/Reset";
 
-import './styles/App.css'
+import './styles/App.css'; 
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="login" element={<Login/>}/>
         <Route path="register" element={<Register/>}/>
+        <Route path="reset" element={<Reset/>}/>
 
         <Route index element={<Navigate to="login" replace />} />
       </Routes>
---
+++ dinopsychweb/src/pages/Reset.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '../styles/Reset.css'
+
+
+const Reset = () => {
+
+    return (
+     <div> 
+
+     </div>
+    );
+}; 
+
+export default Reset; 
