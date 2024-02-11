f113ef6fc441398a98a999706b9b30f449423c9a
piacobelli14 <dinolabsauthentication@outlook.com>
2024-02-10 18:15:51 -0600

    update

--- dinopsychweb/vite.config.js
+++ dinopsychweb/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    // Make sure to add 'jspdf' to the list of external modules
-    external: ['jspdf']
+  build: {
+    rollupOptions: {
+      external: ['jsPDF']
+    }
   }
 })
